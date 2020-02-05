7830763872c277fa4abc0e0d3cca0345a6b30321
Pepe <48911269+DoctorSalt@users.noreply.github.com>
2020-02-05 12:06:46 +0100

    Funciona y tal

--- MatisePractica/src/animales/Principal.java
+++ MatisePractica/src/animales/Principal.java
@@ -23,17 +23,24 @@ public class Principal {
 	
 	
 	public static void main(String[] args) {
-		AnimalesPersintencia.crearPerro(nombrePerro, razaPerro, edadPerro, personalidadPerro, castradoPerro, masculinoPerro);
-		AnimalesPersintencia.crearGato(nombreGato, razaGato, edadGato, personalidadGato, castradoGato, masculinoGato);
-		AnimalesPersintencia.leerPerros();
-		AnimalesPersintencia.leerGatos();		
-		AnimalesPersintencia.modificarPerro(nombrePerro, campoModificar, registroModificar);
-		AnimalesPersintencia.modificarGato(nombreGato,campo2Modificar,registro2Modificar);	
-		AnimalesPersintencia.leerPerros();
-		AnimalesPersintencia.leerGatos();
-		AnimalesPersintencia.deletePerro(nombrePerro);
-		AnimalesPersintencia.deleteGato(nombreGato);
+		//AnimalesPersintencia.crearPerro(nombrePerro, razaPerro, edadPerro, personalidadPerro, castradoPerro, masculinoPerro);
+		//AnimalesPersintencia.crearGato(nombreGato, razaGato, edadGato, personalidadGato, castradoGato, masculinoGato);
+		//AnimalesPersintencia.leerPerros();
+		//AnimalesPersintencia.leerGatos();
+		
+		//AnimalesPersintencia.modificarPerro(nombrePerro, campoModificar, registroModificar);
+		//AnimalesPersintencia.modificarGato(nombreGato,campo2Modificar,registro2Modificar);	
+		
+		
+		//AnimalesPersintencia.leerPerros();
+		//AnimalesPersintencia.leerGatos();
+		
+		//AnimalesPersintencia.deletePerro(nombrePerro);
+		//AnimalesPersintencia.deleteGato(nombreGato);
 		AnimalesPersintencia.leerPerros();
 		AnimalesPersintencia.leerGatos();	
+		
+		
+		
 	}	
 }
